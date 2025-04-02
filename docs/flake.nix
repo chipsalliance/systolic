fe084a659395d4f0e7ff99e559398bf3e68eb419
Huang Rui <vowstar@gmail.com>
2025-04-02 22:53:29 +0800

    build(docs): update flake dependencies and improve typst build
    - Update flake-utils and nixpkgs to latest versions
    - Add pandoc, noto-fonts, and fontconfig as build inputs
    - Improve font handling with custom fontconfig configuration
    - Add markdown to typst conversion via pandoc
    - Make output filename dynamic based on package name
    
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- docs/flake.nix
+++ docs/flake.nix
@@ -2,13 +2,10 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgName = "matmul-spec";
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
       in {
         packages = {
           ${pkgName} = pkgs.stdenv.mkDerivation {
@@ -17,16 +14,53 @@
             src = ./en;
             buildInputs = [
               pkgs.typst
+              pkgs.pandoc
               pkgs.sarasa-gothic
+              pkgs.noto-fonts
+              pkgs.fontconfig
             ];
 
             buildPhase = ''
-              typst compile main.typ "matmul_spec_en.pdf"
+              font_dirs=(
+                "${pkgs.sarasa-gothic}/share/fonts/truetype"
+                "${pkgs.noto-fonts}/share/fonts"
+                # Add more font directories here...
+              )
+              # Create custom fontconfig configuration
+              mkdir -p fontconfig/conf.d
+              cat > fontconfig/conf.d/99-custom-fonts.conf << EOF
+              <?xml version="1.0"?>
+              <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+              <fontconfig>
+                <!-- Add your font directories and set high priority -->
+                <dir>${pkgs.sarasa-gothic}/share/fonts/truetype</dir>
+                <dir>${pkgs.noto-fonts}/share/fonts</dir>
+                <!-- Set font priorities -->
+                <alias binding="strong">
+                  <family>sans-serif</family>
+                  <prefer>
+                    <family>Sarasa Gothic</family>
+                    <family>Noto Sans</family>
+                  </prefer>
+                </alias>
+              </fontconfig>
+              EOF
+              # Use custom configuration
+              export FONTCONFIG_FILE=$(pwd)/fontconfig/conf.d/99-custom-fonts.conf
+              export FONTCONFIG_PATH=${pkgs.fontconfig.out}/etc/fonts
+              for md in *.md; do
+                if [ -f "$md" ]; then
+                  typ_file="$md.typ"
+                  pandoc "$md" -f markdown -t typst -o "$typ_file"
+                fi
+              done
+              export FILE_NAME=$(echo "${pkgName}" | sed 's/-/_/g')_$(basename "$src" | sed 's/^.*-//').pdf
+              typst compile main.typ "$FILE_NAME"
             '';
 
             installPhase = ''
               mkdir -p "$out"
-              cp matmul_spec_en.pdf "$out"
+              cp "$FILE_NAME" "$out"
             '';
           };
         };
@@ -36,13 +70,14 @@
         devShells = {
           default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.${pkgName} ];
-            nativeBuildInputs = with pkgs; [
-              git
-            ];
+            nativeBuildInputs = with pkgs; [ git ];
+            shellHook = ''
+              export FONTCONFIG_PATH="${pkgs.fontconfig.out}/etc/fonts"
+            '';
           };
         };
 
-        defaultPackage = flake-utils.lib.eachDefaultSystem (system: self.packages.${system}.${pkgName});
-      }
-    );
+        defaultPackage = flake-utils.lib.eachDefaultSystem
+          (system: self.packages.${system}.${pkgName});
+      });
 }
